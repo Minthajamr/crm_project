f60f9d5a78f0912519c0d02cd5b34cda9e325098
Minthaj <minthajameer@gmail.com>
2025-11-09 13:17:53 +0530

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProposalController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +20,30 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Customer routes
+    Route::resource('customers', CustomerController::class);
+    Route::patch('customers/{customer}/status', [CustomerController::class, 'updateStatus'])->name('customers.status');
+
+    // Proposal routes
+    Route::resource('proposals', ProposalController::class);
+    Route::patch('proposals/{proposal}/status', [ProposalController::class, 'updateStatus'])->name('proposals.status');
+
+    // Invoice routes
+    Route::resource('invoices', InvoiceController::class);
+    Route::patch('invoices/{invoice}/status', [InvoiceController::class, 'updateStatus'])->name('invoices.status');
+    Route::post('invoices/{invoice}/send', [InvoiceController::class, 'sendInvoice'])->name('invoices.send');
+
+    // Transaction routes
+    Route::get('transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::get('transactions/{transaction}', [TransactionController::class, 'show'])->name('transactions.show');
 });
+
+// Payment routes (accessible without auth for customers)
+Route::get('payment/{invoice}', [PaymentController::class, 'checkout'])->name('payment.checkout');
+Route::post('payment/{invoice}/process', [PaymentController::class, 'processPayment'])->name('payment.process');
+Route::get('payment/{invoice}/success', [PaymentController::class, 'success'])->name('payment.success');
+Route::get('payment/{invoice}/cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
+Route::post('stripe/webhook', [PaymentController::class, 'webhook'])->name('stripe.webhook');
 
 require __DIR__.'/auth.php';
